fe5fb9089721a1484a0152a0ea1b42bf3a3c5d60
CritteRo <critteras@gmail.com>
2021-04-08 23:18:46 +0300

    Improved code

--- [Despre eventuri in Lua]/events/client_script.lua
+++ [Despre eventuri in Lua]/events/client_script.lua
@@ -1,1 +1,13 @@
-TriggerClientEvent('test.event') --apelam la 'test.event'. Acesta va activa toate handle-urile cu ID-ul respectiv.
+RegisterNetEvent('client.event') --eventul 'client.event' este inregistrat, si poate fi chemat din client.
+
+TriggerEvent('client.event') --apelam la 'client.event'. Acesta va activa toate handle-urile cu ID-ul respectiv.
+
+AddEventHandler('client.event', function(arg1, arg2) -- 'client.event' este ID-ul eventului.
+    --'args1' si 'args2' pot fi folosite ca input-uri pentru functie.
+    print('Eventul a fost accesat.')
+    --Aici poate intra orice cod.
+end)
+
+RegisterCommand('sevent', function()
+    TriggerServerEvent('server.event') --apelam la 'server.event' server-side. Acesta va activa toate handle-urile cu ID-ul respectiv.
+end)
--- [Despre eventuri in Lua]/events/server_script.lua
+++ [Despre eventuri in Lua]/events/server_script.lua
@@ -1,9 +1,15 @@
-RegisterNetEvent('test.event') --eventul "test.event" este inregistrat, si poate fi chemat din client.
+RegisterNetEvent('server.event') --eventul 'server.event' este inregistrat, si poate fi chemat din client.
 
-TriggerEvent('test.event') --apelam la 'test.event'. Acesta va activa toate handle-urile cu ID-ul respectiv.
+TriggerEvent('server.event') --apelam la 'server.event'. Acesta va activa toate handle-urile cu ID-ul respectiv.
 
-AddEventHandler('test.event', function(arg1, arg2) -- 'test.event' este ID-ul eventului.
+AddEventHandler('server.event', function(arg1, arg2) -- 'server.event' este ID-ul eventului.
+    local src = source --source este o variabila specifica serverului. Ea este egala cu ID-ul playerului care a apelat la event. 0 = Consola
     --'args1' si 'args2' pot fi folosite ca input-uri pentru functie.
     print('Eventul a fost accesat.')
     --Aici poate intra orice cod.
+end)
+
+RegisterCommand('cevent', function(source, args)
+    local src = source --source este o variabila specifica serverului. Ea este egala cu ID-ul playerului care a apelat la event. 0 = Consola
+    TriggerClientEvent('client.event', src) --apelam la 'client.event' client-side. Este necesar sa precizam ID-ul clientului; 'src' in cazul asta.
 end)
